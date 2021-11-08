f120d338f2c2c1be7be90ba8b2e7206d82fe05ec
xinsix <xinsix@aliyun.com>
2021-11-08 09:23:16 +0800

    上传登录功能

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/login.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      Login
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  padding:0;
+  margin: 0;
+
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;*/
 }
 </style>
---
+++ src/components/login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="_div">
+        <h2>管理员登录</h2>
+        <el-form ref="form" :model="form"  class="form-login">
+            <el-form-item label="">
+                <el-input v-model="form.name" auto-complete="off"  placeholder="用户名"></el-input>
+            </el-form-item>
+            <el-form-item label="">
+                <el-input type="password" v-model="form.pwd" auto-complete="off"  placeholder="密码" show-password="false"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+        data() {
+            return {
+                form: {
+                    name: '',
+                    pwd: ''
+                }
+            }
+        },
+        methods: {
+            onSubmit() {
+                //console.log('submit!');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ._div{
+        margin: 300px;
+    }
+    .form-login{
+        width: 300px;
+        /*margin-left: 35%;*/
+    }
+</style>
