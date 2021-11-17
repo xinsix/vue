38ee9168d2d93ce4171e012dd5adc83558b863e8
xinsix <xinsix@aliyun.com>
2021-11-17 19:17:47 +0800

    添加路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,79 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Index from '../components/page/Index.vue'
+import System from '../components/page/System.vue'
+import GroupIntegral from '../components/page/GroupIntegral.vue'
+import UserIntegral from '../components/page/UserIntegral.vue'
+import AdminManage from '../components/page/manage/AdminManage.vue'
+import GroupManage from '../components/page/manage/GroupManage.vue'
+import UserManage from '../components/page/manage/UserManage.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/Home',
-    name: 'Home',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      //return import(/* webpackChunkName: "about" */ '../components/Home.vue')
-    }
-  }
+    {
+        path: '/',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/Home',
+        name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: function () {
+            return import(/* webpackChunkName: "about" */ '../components/Home.vue')
+        },
+        children: [
+            {
+                path: 'index',
+                component: Index,
+            },
+            {
+                path: 'system',
+                component: System,
+            },
+            {
+                path: 'groupIntegral',
+                component: GroupIntegral,
+            },
+            {
+                path: 'userIntegral',
+                component: UserIntegral,
+            },
+            {
+                path: 'adminManage',
+                component: AdminManage,
+            },
+            {
+                path: 'userManage',
+                component: UserManage,
+            },
+            {
+                path: 'groupManage',
+                component: GroupManage,
+            }
+        ]
+    }/*,
+    {
+        path: '/index',
+        name: 'Index',
+        component: Index
+    },
+    {
+        path: '/home/system',
+        name: 'System',
+        component: System
+    },*/
+
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router
