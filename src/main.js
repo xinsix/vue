d3cd0f5665f0aeb7dfd3cfb454d25c420bb95a7d
xinsix <xinsix@aliyun.com>
2021-11-19 17:14:58 +0800

    mian.js连接后端

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
+import axios from '../node_modules/axios'
+// 设置后端请求地址
+//axios.defaults.baseURL='http://localhost:8089';
+axios.defaults.baseURL='http://120.76.118.8:8089';
 
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
