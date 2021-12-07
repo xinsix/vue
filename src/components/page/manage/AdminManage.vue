f3fb7d6d2ded8c779adafb7b897840908c7fdf12
xinsix <xinsix@aliyun.com>
2021-12-07 14:03:05 +0800

    查询角色

--- src/components/page/manage/AdminManage.vue
+++ src/components/page/manage/AdminManage.vue
@@ -6,14 +6,14 @@
         </el-row>
         <el-divider></el-divider>
         <h3>角色列表</h3>
-        <el-table :data="tableData">
-            <el-table-column prop="date" label="序号">
+        <el-table :data="tableDataAdmin" height="594">
+            <el-table-column type="index" label="序号" width="auto">
             </el-table-column>
-            <el-table-column prop="name" label="角色姓名">
+            <el-table-column prop="adminName" label="角色姓名">
             </el-table-column>
-            <el-table-column prop="address" label="角色账号">
+            <el-table-column prop="adminAccount" label="角色账号">
             </el-table-column>
-            <el-table-column prop="address" label="操作">
+            <el-table-column label="操作">
             </el-table-column>
         </el-table>
     </div>
@@ -21,6 +21,30 @@
 
 <script>
     export default {
+        data() {
+            return {
+                tableDataAdmin: [],
+            };
+        },
+        methods: {
+            handleClick(tab, event) {
+                console.log(tab, event);
+            },
+            init() {
+                var self = this
+                this.$axios.get('/client/admin/search')
+                    .then(function(res) {
+                        console.log(res.data.list);
+                        self.tableDataAdmin = res.data.list
+                    })
+                    .catch(function(err) {
+                        console.log(err)
+                    })
+            },
+        },
+        mounted: function() {
+            this.init()
+        }
     }
 </script>
 
